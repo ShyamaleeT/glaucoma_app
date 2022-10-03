bda2a7cbdfc70100b538d7c796f31611f6853fcb
Thisara Shyamalee <31173511+ShyamaleeT@users.noreply.github.com>
2022-10-03 21:23:49 +0530

    Update glaucocare.py

--- glaucocare.py
+++ glaucocare.py
@@ -45,8 +45,6 @@ def import_and_predict(image_data, model):
     img_reshape = image[np.newaxis,...]
     prediction = model.predict(img_reshape)
     return prediction
-
-
 with st.sidebar:
     choose = option_menu("Content", ["Glaucoma", "Glaucoma Statistics","Glaucoma Analysis Tool"],
                          icons=['house', 'kanban', 'person lines fill'],
@@ -58,7 +56,6 @@ with st.sidebar:
         "nav-link-selected": {"background-color": "#7692c2"},
     }
     )
-
 logo = Image.open('logo.png')
 profile = Image.open('a.jpg')
 if choose == "Glaucoma":
@@ -93,8 +90,7 @@ elif choose == "Glaucoma Statistics":
         font-size:35px ; font-family: 'Cooper Black'; color: #004c94;} 
         </style> """, unsafe_allow_html=True)
         st.markdown('<p class="font"> Glaucoma Statistics </p>', unsafe_allow_html=True)
-
-           
+        
     with col2:               # To display brand logo
         st.image(logo,  width=150)
 
@@ -109,7 +105,6 @@ elif choose == "Glaucoma Statistics":
     st.header("The 10 countries with the highest number of persons with vision loss - 2022")
     st.write("As may be expected, these countries also have the largest populations. China and India together account for 49% of the world’s total burden of blindness and vision impairment, while their populations represent 37% of the global population.")
     AgGrid(df)
-
 
     @st.cache
     def data_upload1():
@@ -137,7 +132,6 @@ elif choose == "Glaucoma Statistics":
     )
     st.altair_chart(bar_chart, use_container_width=True)
 
-
 elif choose == "Glaucoma Analysis Tool":
     col1, col2 = st.columns([0.8, 0.2])
     with col1:               # To display the header text using css style
@@ -157,8 +151,6 @@ elif choose == "Glaucoma Analysis Tool":
     #model_path = os.path.join(my_path, "sep_5.h5")
 
     model = tf.keras.models.load_model('model.h5', compile=False)
-    
-    
     label_dict={1:'Glaucoma', 0:'Normal'}
 
     file = st.file_uploader("Please upload an image(jpg/png/jpeg/bmp) file", type=["jpg", "png", "jpeg", "bmp"])
@@ -193,8 +185,6 @@ elif choose == "Glaucoma Analysis Tool":
             (heatmap, output) = cam.overlay_heatmap(heatmap, opencv_image, alpha=0.5)
             heatmap = imutils.resize(heatmap, width=100)
             st.image(heatmap,channels="BGR",use_column_width=True)
-
-
         with col_c:
             st.subheader("Visualized image")
             last_conv_layer= "conv5_block3_out" 
@@ -223,18 +213,14 @@ elif choose == "Glaucoma Analysis Tool":
 
         Normal_prob = "{:.2%}".format(1-pred)
         Glaucoma_prob = "{:.2%}".format(pred)
-
-
         if(pred> 0.5):
             st.markdown(f'<h1 style="color:Red;font-size:35px;">{""" Glaucoma Eye"""}</h1>', unsafe_allow_html=True)
             st.text("The area in the image that is highlighted is thought to be glaucomatous.")
-            
         else:
             st.markdown(f'<h1 style="color:Blue;font-size:35px;">{"""Healthy Eye"""}</h1>', unsafe_allow_html=True)
                 #st.write("""### You are affected by Glaucoma. Please consult an ophthalmologist as soon as possible.""" )
 
         st.subheader('Prediction Probability')
-
         col1, col2 = st.columns(2)
         col1.metric("Glaucoma", Glaucoma_prob)
         col2.metric("Normal", Normal_prob)
